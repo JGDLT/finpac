e9921d91b14bc4c833f2f822e3f9d37e1d8fc0f9
JGDLT <91678501+JGDLT@users.noreply.github.com>
2022-04-08 09:58:06 -0600

    Create ghost.h

---
+++ ghost.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "SFML/Graphics.hpp"
+#include"globals.h"
+class ghost {
+
+public:
+	ghost();
+	void initGhost(int x, int y, int dir, bool dead, int Gspeed, int Gcolor);
+	bool isdead();
+	void drawGhost(sf::RenderWindow& window);
+	void printInfo();
+	void chase1(int x, int y, int field[12][12]);
+	void chase2(int x, int y, int field[12][12]);
+	void chase3(int x, int y, int field[12][12]);
+	void chase4(int x, int y, int field[12][12]);
+	bool getPacman(int x, int y);
+	int getXpos();
+	int getYpos();
+
+private:
+
+	int xpos;
+	int ypos;
+	char color;
+	int direction;
+	bool dead;
+	int speed;
+	int offset;
+	sf::CircleShape shape;
+
+};
